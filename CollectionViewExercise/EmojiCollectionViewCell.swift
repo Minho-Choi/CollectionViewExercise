8ae52043334c9128d7785fb2a2dec70f71f6f15b
Minho-Choi <2153373@naver.com>
2020-07-29 17:32:12 +0900

    image drag n drop function added(fail)

--- CollectionViewExercise/EmojiCollectionViewCell.swift
+++ CollectionViewExercise/EmojiCollectionViewCell.swift
@@ -10,5 +10,32 @@ import UIKit
 
 class EmojiCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    var aspectRatio: NSString {
+        if let imageSize = imageView.image?.size {
+            return NSString(string: String(Float(imageSize.height / imageSize.width)))
+        } else {
+            return "1"
+        }
+    }
+    
+    var imageURL: URL? {
+        didSet {
+            if let url = self.imageURL {
+                spinner.startAnimating()
+                DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+                    let urlContents = try? Data(contentsOf: url)
+                    DispatchQueue.main.async { // UI 코드이므로 main에 넣어줌
+                        if let imageData = urlContents, url == self?.imageURL { //이미지 로딩 후 다른 요청이 생겨 url이 바뀌는 경우 방지
+                            self?.imageView.image = UIImage(data: imageData)
+                            self?.spinner.stopAnimating()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
 }
--- CollectionViewExercise/ViewController.swift
+++ CollectionViewExercise/ViewController.swift
@@ -17,12 +17,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
 
     // Model
-    var emojiSet = [1.5, 0.5, 0.77, 2, 1, 0.8, 0.33, 0.8, 1.1, 1.9, 0.6, 0.7, 1]
-    var imageURLArray = [URL]()
-    
-    private var font: UIFont {
-        return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(128.0))
-    }
+    var imageURLStringSet = ["http://www.random-art.org/media/img/Red_Demo.jpg", "http://www.random-art.org/media/img/Demo_Green.jpg",
+    "http://www.random-art.org/media/img/Demo_Blue.jpg",
+    "http://www.random-art.org/media/img/Demo_Red.jpg",
+    "http://www.random-art.org/media/img/Blue_Demo.jpg"]
     
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
@@ -32,26 +30,37 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             collectionView.dropDelegate = self
         }
     }
+    // 화면 방향 전환 시 뷰 다시 그리기
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if let flowLayout = self.collectionView!.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.invalidateLayout()
+        }
+    }
+    
     // Collection View Setting Methods
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return emojiSet.count
+        return imageURLStringSet.count
     }
     // cell size set
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let aspectRatio = CGFloat(emojiSet[indexPath.item])
-        let width = view.frame.width / 4.2
-        let height = width * aspectRatio
-        return CGSize(width: width, height: height)
+        //let aspectRatio = CGFloat(imageARStringSet[indexPath.item])
+        if let ARatio = (collectionView.cellForItem(at: indexPath) as? EmojiCollectionViewCell)?.aspectRatio {
+            let width = view.bounds.width / 2.2
+            let height = width * CGFloat(ARatio.floatValue)
+            print("global aspect ratio: \(width), \(height)")
+            return CGSize(width: width, height: height)
+        } else {
+            return CGSize(width: view.bounds.width / 2.2, height: view.bounds.width / 2.2)
+        }
     }
     // cell shape & inner information set
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmojiCell", for: indexPath)
         if let emojiCell = cell as? EmojiCollectionViewCell {
-            let text = NSAttributedString(string: String(emojiSet[indexPath.item]), attributes: [.font:font])
-            emojiCell.emojiLabel.attributedText = text
+            emojiCell.imageURL = URL(string: imageURLStringSet[indexPath.item])
             emojiCell.layer.borderWidth = 3.0
-            emojiCell.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            emojiCell.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         }
         return cell
     }
@@ -59,14 +68,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     // available types view can handle
     func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
-//        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
-        return session.canLoadObjects(ofClass: NSString.self)
+        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
     }
     
     // move or copy
     func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
         let isSelf = (session.localDragSession?.localContext as? UICollectionView) == collectionView
-
         return UICollectionViewDropProposal(operation: isSelf ? .move : .copy, intent: .insertAtDestinationIndexPath)
     }
     
@@ -78,12 +85,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             // 아이템이 본래 있던 자리(드래그가 시작된 자리)
             if let sourceIndexPath = item.sourceIndexPath {
                 // 드래그한 아이템의 내용(attributedString)
-                if let attributedString = item.dragItem.localObject as? NSAttributedString {
+                if let imageURL = item.dragItem.localObject as? NSURL {//, let imageAR = item.dragItem.localObject as? Double{
                     // performBatchUpdates의 효과는 모델과 뷰를 동시에 수정함으로써 싱크 유지
                     collectionView.performBatchUpdates({
                         // 모델 편집 - 본래 위치에서 삭제 후 새로운 위치에 추가
-                        emojiSet.remove(at: sourceIndexPath.item)
-                        emojiSet.insert(NSString(string: attributedString.string).doubleValue, at: destinationIndexPath.item)
+                        imageURLStringSet.remove(at: sourceIndexPath.item)
+                        imageURLStringSet.insert(imageURL.absoluteString!, at: destinationIndexPath.item)
+//                        imageARStringSet.remove(at: sourceIndexPath.item)
+//                        imageARStringSet.insert(., at: )
                         // 뷰 편집 - 전체 뷰를 업데이트할 경우 애니메이션 실행되지 않으므로 부자연스러움
                         // 따라서 아이템을 하나씩 지우고 더하는 메소드를 사용
                         collectionView.deleteItems(at: [sourceIndexPath])
@@ -99,14 +108,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                     to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath, reuseIdentifier: "DropPlaceholderCell")
                 )
                 // 드래그 아이템을 attributedString으로 받는 과정 - 당연히 error 발생가능
-                item.dragItem.itemProvider.loadObject(ofClass: NSAttributedString.self) { (provider, error) in
+                item.dragItem.itemProvider.loadObject(ofClass: URL.self) { (provider, error) in
                     // main 큐에서 코드 수행 - 뷰와 관련된 코드이므로
                     DispatchQueue.main.async {
                         // 받는 데 성공했을 경우
-                        if let attributedString = provider as? NSAttributedString {
+                        if let imageURL = provider {
                             // 플레이스홀더 셀을 내용이 들어간 셀로 치환
                             placeholderContext.commitInsertion(dataSourceUpdates: { insertionIndexPath in
-                                self.emojiSet.insert(NSString(string: attributedString.string).doubleValue, at: insertionIndexPath.item)
+                                self.imageURLStringSet.insert(imageURL.absoluteString, at: insertionIndexPath.item)
                             })
                             // 받는 데 실패했을 경우
                         } else {
@@ -118,7 +127,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             }
         }
     }
-
+    // Drag
 
     func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
         session.localContext = collectionView
@@ -131,10 +140,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     private func dragItems(at indexPath: IndexPath) -> [UIDragItem] {
         // cellForItem: Returns the visible cell object at the specified index path.
-        if let attributedString = (collectionView.cellForItem(at: indexPath) as? EmojiCollectionViewCell)?.emojiLabel.attributedText {
-            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: attributedString))
-            dragItem.localObject = attributedString
-            return [dragItem]
+        if let imageURL = (collectionView.cellForItem(at: indexPath) as? EmojiCollectionViewCell)?.imageURL, let imageAR = (collectionView.cellForItem(at: indexPath) as? EmojiCollectionViewCell)?.aspectRatio {
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: imageURL as NSItemProviderWriting))
+            let dragItem2 = UIDragItem(itemProvider: NSItemProvider(object: imageAR as NSItemProviderWriting))
+            dragItem.localObject = true
+            dragItem2.localObject = true
+            return [dragItem, dragItem2]
         } else {
             return []
         }
